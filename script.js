525485ff04383442123f416676cdf39d913d4a6e
0x-UG <okekecee12@gmail.com>
2023-10-24 15:09:32 +0100

    made minor changes

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const container = document.querySelector('#container');
 
-/* const div = document.createElement('div');
-div.classList.add ('grid-item');
-container.appendChild(div);
-
-const div2 = document.createElement('div');
-div2.classList.add('grid-item');
-container.appendChild(div2);
-
-*/
 
 for (let i = 1; i <= 256; i++) {
         const gridItem = document.createElement('div');
