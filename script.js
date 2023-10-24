997097445537055d61884c1c6a06c093793112d0
0x-UG <okekecee12@gmail.com>
2023-10-24 13:12:51 +0100

    query selected the container

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+const div = document.createElement('div');
+div.style.border = '1px solid black';
+div.style.width = '10px';
+div.style.height = '10px';
+
+container.appendChild(div);
