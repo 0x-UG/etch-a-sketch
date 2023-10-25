cb8b3e18600484c12e1bff0ac4ad71709a7a0d3b
0x-UG <okekecee12@gmail.com>
2023-10-25 10:38:54 +0100

    created function that takes allows user to get dimension of the board or start with default 16x16 board

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
        createBoard(16); 
+       let popup = document.querySelector('#popup');
+       popup.addEventListener('click', function() {
+        let size = getUserInput();
+        createBoard(size);
+       });
 })
 
 
@@ -13,7 +18,19 @@ function createBoard (size) {
 
         for (let i = 0; i < dimension; i++) {
                 let div = document.createElement('div');
-                div.style.backgroundColor = 'blue';
                 container.insertAdjacentElement('beforeend', div);
+        }
+}
+
+function getUserInput () {
+        let input = prompt("Input a size");
+        let message = document.querySelector('#message');
+        if (input == '') {
+            message.innerHTML = 'Please provide a number';    
+        } else if (input < 0 || input > 100) {
+                message.innerHTML = 'Invalid dimension! Input a number between 2 and 100';
+        } else {
+                message.innerHTML = 'Have fun!!';
+                return input;
         }
 }
