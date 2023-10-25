a7709782c786ae14d83620bbb57f86785b08a83f
0x-UG <okekecee12@gmail.com>
2023-10-25 10:13:12 +0100

    created board using grid

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const container = document.querySelector('#container');
+document.addEventListener('DOMContentLoaded', function() {
+       createBoard(16); 
+})
 
 
-/* for (let i = 1; i <= 256; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        container.appendChild(gridItem);
+function createBoard (size) {
+        let container = document.querySelector('#container');
+
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+        let dimension = size * size;
+
+        for (let i = 0; i < dimension; i++) {
+                let div = document.createElement('div');
+                div.style.backgroundColor = 'blue';
+                container.insertAdjacentElement('beforeend', div);
+        }
 }
-*/
-
