5e447ba4d1ad31a829dff8875a5ae3e720e37b45
0x-UG <okekecee12@gmail.com>
2023-12-26 12:16:29 +0100

    trying to fix deployment issue

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Etch-A-Sketch
 
-In this project, I'll be creating a pretty neat toy to flex my DOM manipulation skills. I'm going to build a browser version of something between a sketchpad and an Etch-A-Sketch.
+In this project, I'll be creating a pretty neat toy to flex my DOM manipulation skills. I'm going to build a browser version of something between a sketchpad and an Etch-A-Sketch using HTML, CSS and JS.
 
 ## What this project is about
+
 This project is a mini sketch book that starts out as an empty grid box but changes color whenever the mouse hovers over the grid similar to how a pen would leave a trail on a paper.
 The user should be able to change the number of squares on each side of the grid. Once selected, the existing grid should be removed and a new grid should be generated in the same space as before so that you have a new sketch pad.
-
-
--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-let color = 'black';
+let color = "black";
 
 let click = false;
 
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector("body").addEventListener("click", function (e) {
+		if (e.target.tagName != "BUTTON") {
+			click = !click;
+			let draw = document.querySelector("#draw");
+			if (click) {
+				draw.innerHTML = "Now you can draw. Have fun!!";
+			} else {
+				draw.innerHTML = "You can't draw :(";
+			}
+		}
+	});
 
-document.addEventListener('DOMContentLoaded', function() {
-       document.querySelector('body').addEventListener('click', function(e) {
-        if(e.target.tagName != "BUTTON") {
-                click = !click;
-                let draw = document.querySelector('#draw');
-                if (click) {
-                        draw.innerHTML = 'Now you can draw. Have fun!!';
-                } else {
-                        draw.innerHTML = 'You can\'t draw :(';
-                }
-        }
-       })
+	createBoard(16);
+	let popup = document.querySelector("#popup");
+	popup.addEventListener("click", function () {
+		let size = getUserInput();
+		createBoard(size);
+	});
+});
 
-        createBoard(16); 
-       let popup = document.querySelector('#popup');
-       popup.addEventListener('click', function() {
-        let size = getUserInput();
-        createBoard(size);
-       });
-})
+function createBoard(size) {
+	let container = document.querySelector("#container");
 
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function createBoard (size) {
-        let container = document.querySelector('#container');
+	let dimension = size * size;
 
-        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-        let dimension = size * size;
-
-        for (let i = 0; i < dimension; i++) {
-                let div = document.createElement('div');
-                div.addEventListener('mouseover', colorDiv);
-                container.insertAdjacentElement('beforeend', div);
-        }
+	for (let i = 0; i < dimension; i++) {
+		let div = document.createElement("div");
+		div.addEventListener("mouseover", colorDiv);
+		container.insertAdjacentElement("beforeend", div);
+	}
 }
 
-function getUserInput () {
-        let input = prompt("Input a size");
-        let message = document.querySelector('#message');
-        if (input == '') {
-            message.innerHTML = 'Please provide a number';    
-        } else if (input < 0 || input > 100) {
-                message.innerHTML = 'Invalid dimension! Input a number between 2 and 100';
-        } else {
-                return input;
-        }
+function getUserInput() {
+	let input = prompt("Input a size");
+	let message = document.querySelector("#message");
+	if (input == "") {
+		message.innerHTML = "Please provide a number";
+	} else if (input < 0 || input > 100) {
+		message.innerHTML =
+			"Invalid dimension! Input a number between 2 and 100";
+	} else {
+		return input;
+	}
 }
 
-
-function colorDiv () {
-      if (click) {
-        if(color == "random") {
-                this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-         } else {
-                 this.style.backgroundColor = 'black';
-         }
-      }  
+function colorDiv() {
+	if (click) {
+		if (color == "random") {
+			this.style.backgroundColor = `hsl(${
+				Math.random() * 360
+			}, 100%, 50%)`;
+		} else {
+			this.style.backgroundColor = "black";
+		}
+	}
 }
 
-function setColor (colorChoice) {
-        color = colorChoice;
+function setColor(colorChoice) {
+	color = colorChoice;
 }
 
-function resetBoard () {
-        let divs = document.querySelectorAll('div');
-        divs.forEach((div) => div.style.backgroundColor = 'white');
+function resetBoard() {
+	let divs = document.querySelectorAll("div");
+	divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
